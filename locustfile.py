6c6b27caf528a1ed1063b15359e64263d423a954
atomkowicz <agntomk@gmail.com>
2020-11-10 17:44:36 +0100

    Feature: add locust file

---
+++ locustfile.py
@@ -0,0 +1,12 @@
+import time
+from locust import HttpUser, task, between
+
+class QuickstartUser(HttpUser):
+    wait_time = between(1, 2)
+
+    @task
+    def index_page(self):
+        self.client.get("/")
+        response = self.client.post("/predict", json={"CHAS":{"0":0},"RM":{"0":6.575},"TAX":{"0":296.0},"PTRATIO":{"0":15.3},"B":{"0":396.9},"LSTAT":{"0":4.98}})
+        print("Response status code:", response.status_code)
+        print("Response text:", response.text)
